0b7056649958f0b7d8b4a84b244dbd6f1a61da96
Janusz Kobylarz <j.kobylarz@live.com>
2024-06-07 20:50:32 +0200

    Test employee

---
+++ src/components/EmployeeItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="employee in employees" :key="employee.id">
+      {{ employee.name }}
+    </li>
+  </ul>
+</template>
+<script setup>
+import { ref } from 'vue'
+const employees = ref([
+  { id: 1, name: 'Janusz', surname: 'Kobylarz', position: 'PREZES', parent_id: null },
+  { id: 2, name: 'Imi\u01191', surname: 'Nazwisko1', position: 'Tester', parent_id: null },
+  { id: 3, name: 'Imi\u01192', surname: 'Nazwisko2', position: 'Programista', parent_id: null },
+  { id: 4, name: 'Imi\u01193', surname: 'Nazwisko3', position: 'Programista', parent_id: 1 }
+])
+</script>
